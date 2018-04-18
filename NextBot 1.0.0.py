10fb04ee47a892fdf9e17f730f501cacb315375e
misteurLeu <jleu@student.42.fr>
2018-04-18 17:59:26 +0200

    meilleur message de debut de stream, erreur correction quand after.game is none

--- NextBot 1.0.0.py
+++ NextBot 1.0.0.py
@@ -6,11 +6,11 @@
 
 import asyncio, discord, os
 
-user_bot = "LouveBot" #Mettez dans cette variable le pseudo du bot.
-token = os.environ['BOT_TOKEN'] #Mettez dans cette variable le token du bot
-trust = os.environ['TRUST_USER'].split(',') #admins du bot
+user_bot = "LouveBot"  # Mettez dans cette variable le pseudo du bot.
+token = os.environ['BOT_TOKEN']  # Mettez dans cette variable le token du bot
+trust = os.environ['TRUST_USER'].split(',')  # admins du bot
 ranks = False
-f = open('streamers.txt', 'r', encoding='utf-8')
+f = open('streamers.txt', 'w+', encoding='utf-8')
 streamers = f.read().split("\n")
 f.close()
 
@@ -20,12 +20,16 @@ lang = "fr"
 
 print("LouveBot " + ver + " " + lang)
 
+
 @client.event
 @asyncio.coroutine
 def on_member_update(before, after):
     streamChannel = [chan for chan in after.server.channels if chan.id == "405352007302643712"][0]
-    if str(after) in streamers and after.game.url is not None and 'twitch.tv' in after.game.url:
-        yield from client.send_message(streamChannel, str(after)+' est en live sur du '+str(after.game)+' '+after.game.url)
+    if after.game is not None and after.game.url is not None:
+        if str(after) in streamers and 'twitch.tv' in after.game.url:
+            yield from client.send_message(streamChannel, str(after).split('#')[0] + ' est en live GOGOGOGO :\n' + str(
+                after.game) + ' ' + after.game.url)
+
 
 @client.event
 @asyncio.coroutine
@@ -72,7 +76,7 @@ def on_message(message):
         msgs.write(msgs_r)
         msgs.close()
 
-#Début des commandes
+    # Début des commandes
 
     if command == "!test":
         yield from client.send_message(message.channel, user)
@@ -81,61 +85,70 @@ def on_message(message):
     if command == "!whereami":
         yield from client.send_message(message.channel, message.server)
         yield from client.send_message(message.channel, message.channel.id)
-        
-    if command == "!commandtest": #Copiez ce code pour créer une commande
+
+    if command == "!commandtest":  # Copiez ce code pour créer une commande
         yield from client.send_message(message.channel, "Texte à envoyer.")
 
-    if command == "!saymyname": #Copiez ce code pour créer une commande
+    if command == "!saymyname":  # Copiez ce code pour créer une commande
         yield from client.send_message(message.channel, user)
 
-    if command == "!ban" and trusted and not pm: #Cette commande sert à bannir un utilisateur, and trusted veux dire que la commande est restreinte et que seuls les utilisateurs en trust peuvent utiliser la commande et and not pm veux dire que la commande n'est pas utilisable en PM.
-        id_user = message.server.get_member_named(params[1]) #La variable params[1] est le premier paramètre entré par l'utilisateur, cette ligne sert à donner l'identifiant de l'utilisateur à partir du pseudo. Pour avoir l'identifiant d'un utilisateur, un serveur ou d'autre chose, vous devez activer le mode développeur
+    if command == "!ban" and trusted and not pm:  # Cette commande sert à bannir un utilisateur, and trusted veux dire que la commande est restreinte et que seuls les utilisateurs en trust peuvent utiliser la commande et and not pm veux dire que la commande n'est pas utilisable en PM.
+        id_user = message.server.get_member_named(params[
+                                                      1])  # La variable params[1] est le premier paramètre entré par l'utilisateur, cette ligne sert à donner l'identifiant de l'utilisateur à partir du pseudo. Pour avoir l'identifiant d'un utilisateur, un serveur ou d'autre chose, vous devez activer le mode développeur
         try:
-            yield from client.ban(id_user, int(params[2])) #cette ligne sert à bannir l'utilisateur grâce à la variable id_user qui représente l'identifiant de l'utilisateur à bannir
-        except IndexError: #si le nombre de messages à supprimer n'est pas mis (en tapant juste !ban utilisateur), le bot bannira l'utilisateur mais ne supprimera aucun message
+            yield from client.ban(id_user, int(params[
+                                                   2]))  # cette ligne sert à bannir l'utilisateur grâce à la variable id_user qui représente l'identifiant de l'utilisateur à bannir
+        except IndexError:  # si le nombre de messages à supprimer n'est pas mis (en tapant juste !ban utilisateur), le bot bannira l'utilisateur mais ne supprimera aucun message
             yield from client.ban(id_user, 0)
 
-    if command == "!google": #Voir la commande !bing
+    if command == "!google":  # Voir la commande !bing
         yield from client.send_message(message.channel, "https://www.google.com/#q=" + "+".join(params[1:]))
 
-    if command == "!kick" and trusted and not pm: #Voir la commande !ban
+    if command == "!kick" and trusted and not pm:  # Voir la commande !ban
         id_user = message.server.get_member_named(params[1])
         yield from client.kick(id_user)
 
-    if (command == "!purge" or command == "!clear") and trusted and not pm: #Cette commande sert à effacer les messages, en tapant !purge 10, le bot supprimera les 10 derniers messages.
-        yield from client.purge_from(message.channel, limit = int(params[1])) #Cette ligne sert à supprimer les messages avec params[1] qui est le premier paramètre (le nombre de messages), il y a int(params[1]) car le paramètre doit être converti en un nombre.
+    if (
+            command == "!purge" or command == "!clear") and trusted and not pm:  # Cette commande sert à effacer les messages, en tapant !purge 10, le bot supprimera les 10 derniers messages.
+        yield from client.purge_from(message.channel, limit=int(params[
+                                                                    1]))  # Cette ligne sert à supprimer les messages avec params[1] qui est le premier paramètre (le nombre de messages), il y a int(params[1]) car le paramètre doit être converti en un nombre.
 
-    if (command == "!quit" or command == "!exit") and trusted: #Cette commande sert à fermer le bot
+    if (command == "!quit" or command == "!exit") and trusted:  # Cette commande sert à fermer le bot
         yield from client.close()
 
-    if command == "!role_user_add" and trusted and not pm: #Cette commande sert à ajouter un rôle à un utilisateur
+    if command == "!role_user_add" and trusted and not pm:  # Cette commande sert à ajouter un rôle à un utilisateur
         member = message.server.get_member_named(params[1])
-        role = discord.utils.get(message.server.roles, name = " ".join(params[2:])) #cette ligne sert à récupérer le rôle de l'utilisateur à ajouter, " ".join(params[2:]) est le nom du rôle
-        yield from client.add_roles(member, role) #cette ligne sert à appliquer l'ajout du rôle à l'utilisateur et member est l'identifiant de l'utilisateur et role est l'identifiant du rôle
+        role = discord.utils.get(message.server.roles, name=" ".join(params[
+                                                                     2:]))  # cette ligne sert à récupérer le rôle de l'utilisateur à ajouter, " ".join(params[2:]) est le nom du rôle
+        yield from client.add_roles(member,
+                                    role)  # cette ligne sert à appliquer l'ajout du rôle à l'utilisateur et member est l'identifiant de l'utilisateur et role est l'identifiant du rôle
 
-    if command == "!role_user_remove" and trusted and not pm: #Cette commande sert à retirer un rôle à un utilisateur
+    if command == "!role_user_remove" and trusted and not pm:  # Cette commande sert à retirer un rôle à un utilisateur
         member = message.server.get_member_named(params[1])
-        role = discord.utils.get(message.server.roles, name = " ".join(params[2:]))
-        yield from client.remove_roles(member, role) #cette ligne sert à retirer le rôle d'un utilisateur, son fonctionnement est quasi-identique à part qu'elle fait l'inverse (elle retire le rôle au lieu de l'ajouter)
+        role = discord.utils.get(message.server.roles, name=" ".join(params[2:]))
+        yield from client.remove_roles(member,
+                                       role)  # cette ligne sert à retirer le rôle d'un utilisateur, son fonctionnement est quasi-identique à part qu'elle fait l'inverse (elle retire le rôle au lieu de l'ajouter)
 
-    if command == "!roles" and trusted and not pm: #Cette commande sert à lister les rôles sur le serveur
-        for role in message.server.roles: #cette ligne est une boucle et sert à mettre dans la variable role la liste des rôles du serveur avec message.server.roles
+    if command == "!roles" and trusted and not pm:  # Cette commande sert à lister les rôles sur le serveur
+        for role in message.server.roles:  # cette ligne est une boucle et sert à mettre dans la variable role la liste des rôles du serveur avec message.server.roles
             yield from client.send_message(message.channel, role.id + " : " + role.name)
 
-    if command == "!unban" and trusted and not pm: #Cette commande sert à débannir un utilisateur
+    if command == "!unban" and trusted and not pm:  # Cette commande sert à débannir un utilisateur
         id_user = message.server.get_member_named(params[1])
-        yield from client.unban(message.server, id_user) #pour débannir un utilisateur, il faut l'identifiant du serveur avec message.serveur et l'identifiant de l'utilisateur (voir !ban)
+        yield from client.unban(message.server,
+                                id_user)  # pour débannir un utilisateur, il faut l'identifiant du serveur avec message.serveur et l'identifiant de l'utilisateur (voir !ban)
 
-    if command == "!say" and trusted: #Cette commande sert à envoyer un message sur un channel du serveur, le paramètre 1 doit être l'identifiant du channel et après, on doit mettre le message (exemple : !say 1234567890 Bonjour !)
+    if command == "!say" and trusted:  # Cette commande sert à envoyer un message sur un channel du serveur, le paramètre 1 doit être l'identifiant du channel et après, on doit mettre le message (exemple : !say 1234567890 Bonjour !)
         yield from client.send_message(client.get_channel(params[1]), " ".join(params[2:]))
 
     if command == "!say_user" and trusted:
         if params[2].lower() == params[2].upper():
             yield from client.send_message(client.get_server(params[1]).get_member(params[2]), " ".join(params[3:]))
         else:
-            yield from client.send_message(client.get_server(params[1]).get_member_named(params[2]), " ".join(params[3:]))
+            yield from client.send_message(client.get_server(params[1]).get_member_named(params[2]),
+                                           " ".join(params[3:]))
 
-    if command == "!ver": #Cette commande envoit la version du bot.
+    if command == "!ver":  # Cette commande envoit la version du bot.
         yield from client.send_message(message.channel, "NextBot " + ver + " " + lang)
 
     if command == "!streamadd" and trusted:
@@ -147,9 +160,11 @@ def on_message(message):
             f2.write(userToAdd + "\n")
             f2.close()
             streamers.append(userToAdd)
-            yield from client.send_message(message.channel, userToAdd + " a bien été ajouté a la liste des streams a afficher")
+            yield from client.send_message(message.channel,
+                                           userToAdd + " a bien été ajouté a la liste des streams a afficher")
         elif userToAdd in memberList:
-            yield from client.send_message(message.channel, userToAdd + " fais déjàs partie de la liste des streams a afficher")
+            yield from client.send_message(message.channel,
+                                           userToAdd + " fais déjàs partie de la liste des streams a afficher")
         else:
             yield from client.send_message(message.channel, "Erreur dans le nom d'utilisateur")
 
@@ -166,10 +181,12 @@ def on_message(message):
             streamers.clear()
             for s in newStreamList:
                 streamers.append(s)
-            yield from client.send_message(message.channel, userToRm + " a bien été supprimé a la liste des streams a afficher")
+            yield from client.send_message(message.channel,
+                                           userToRm + " a bien été supprimé a la liste des streams a afficher")
         else:
             yield from client.send_message(message.channel, "Erreur dans le nom d'utilisateur")
-        
-#Fin des commandes
+
+
+# Fin des commandes
 
 client.run(token)
